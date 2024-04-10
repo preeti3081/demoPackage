70373a59033b95c2fec922fb93595a7b6095a881
preeti <preeti@samarth.ac.in>
2024-04-10 12:06:53 +0530

    First demo

---
+++ src/Demo1.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\demoPackage;
+
+class Demo
+{
+    public static function getdata($name){
+        echo $name;
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'preeti/helloworld',
+        'pretty_version' => '2.0.49',
+        'version' => '2.0.49.0',
+        'reference' => 'c083f404c25ee4113dbc8e5dba4b0c45c79375a5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'preeti/helloworld' => array(
+            'pretty_version' => '2.0.49',
+            'version' => '2.0.49.0',
+            'reference' => 'c083f404c25ee4113dbc8e5dba4b0c45c79375a5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
